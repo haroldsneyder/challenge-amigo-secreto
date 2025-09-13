2300aa71df141f267ab25093d91763a96afe5900
haroldsneyder <ariasjuan85@hotmail.com>
2025-09-13 08:48:39 -0500

    Se modifican los métodos agregarAmigo y se elimina el metodo asignar texto elemento, se agrega el metodo mostrarlista

--- app.js
+++ app.js
@@ -1,46 +1,37 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-//Método para cargar los valores a la lista visible
-function asignarTextoElemento(elemento, texto){ //Indico que se recibirán dos parámetros
 
-    //elemento del HTML
-    let elementoHTML = document.querySelector(elemento);
+// Método para cargar la lista visual
+function mostrarLista() {
 
-    //Texto efectivo
-    elementoHTML.innerHTML = texto;
+    let lista = document.querySelector('#listaAmigos');
 
-    return;
-
+    lista.innerHTML = amigos.map(amigo => `<li>${amigo}</li>`).join('');
 }
 
 //Método para tomar el valor ingresado por el usuario
 function agregarAmigo(){    
 
     //Se toma el valor del objeto y se carga en la variable local
-    let nombreAmigo = document.getElementById('amigo');
+    let nombreAmigo = document.getElementById('amigo').value.trim();
 
     //Se valida que el objeto no se encuentre vacío
-    if(typeof nombreAmigo === "String" && nombreAmigo.lenght === 0){
+    if(nombreAmigo.length === 0){
 
         //Se indica por consola que el nombre está vacío
         alert('Debe indicar el nombre');
 
-    }else if(nombreAmigo === null){
-
-        //Se indica por consola que la cadena es nula
-        alert('Debe ingresar un valor');
-
-    }else{
+        return;
+    }
         
-        //Cargo el nombre del amigo dentro del arreglo
-        amigos.push(nombreAmigo);
+    //Cargo el nombre del amigo dentro del arreglo
+    amigos.push(nombreAmigo);
 
-        //Se carga el valor a la lista visual
-        asignarTextoElemento('listaAmigos', `${amigos}`);
-    }    
+    mostrarLista();
 
-    return;
+    // limpiar input
+    document.getElementById('amigo').value = '';    
 
 }
 
