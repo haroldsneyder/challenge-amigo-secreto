874f49a3a6b6437e58ae4c732f6d1d73f2f73d73
haroldsneyder <ariasjuan85@hotmail.com>
2025-09-13 10:14:27 -0500

    Se modifica el método sortearAmigo y se agregan los comentarios de línea correspondientes.

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ let amigos = [];
 // Método para cargar la lista visual
 function mostrarLista() {
 
+    //Se captura la información del objeto listaAmigos
     let lista = document.querySelector('#listaAmigos');
 
+    //Se agrega a la lista el objeto li para cada nuevo "amigo"
     lista.innerHTML = amigos.map(amigo => `<li>${amigo}</li>`).join('');
 }
 
@@ -38,4 +40,25 @@ function agregarAmigo(){
 //Método para realizar el sorteo
 function sortearAmigo(){
     
+    //Se valida que el arreglo no esté vacío
+    if(amigos.length == 0){
+
+        alert("No hay amigos en la lista para sortear");
+
+        return;
+
+    }
+
+    //Se genera un número entero aleatorio entre 0 y el tamaño del arreglo (sin incluirlo)
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    
+    //Con ese índice, se obtiene el nombre correspondiente dentro del arreglo
+    let nombreAleatorio = amigos[indiceAleatorio];
+
+    //Se busca el elemento con id "resultado"
+    let resultado = document.querySelector('#resultado');
+
+    //Se reemplaza el contenido del elemento con el nombre sorteado, dentro de un <li> y le indico que lo deje en negrilla.
+    resultado.innerHTML = `<li>El amigo secreto es: <strong>${nombreAleatorio}</strong></li>`;
+
 }
